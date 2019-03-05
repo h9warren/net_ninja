5738c684da2cec43f9db960097d847ebe48cfcdb
Hunter Warren <hunter.warren@gmail.com>
2019-03-04 22:30:22 -0600

    using axios for a get request. generates a blog feed populated with posts.

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component} from 'react';
+import axios from 'axios'
 
-const Home = () => {
-	return (
-	<div className="container">
-		<h4 className="container center">Home</h4>
-		<p className="">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque possimus error ratione illo explicabo fuga illum quis, doloribus ab, consectetur nulla! Assumenda, atque, unde ut adipisci voluptatum laborum quia velit.
-		</p>
-	</div>
-	)
+class Home extends Component {
+	state = {
+		posts: []
+	};
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+		.then( res => {
+			this.setState({
+				posts: res.data.slice(0,10)
+			});
+			console.log(this.state);
+		});
+	}
+	render() {
+		const { posts } = this.state;
+		const postList = posts.length ? (
+			posts.map((post) => {
+				return (
+					<div className="post card" key={post.id}>
+						<div className="card-content">
+							<span className="card-title">
+								<p>{post.body}</p>
+							</span>
+						</div>
+					</div>	
+				)
+			})
+			) : (
+			<div className="center">No Posts Yet</div>
+			)
+
+		return (
+				<div className="container">
+					<h4 className="container center">Home</h4>
+				{postList}
+				</div>
+			)
+		}
 }
 export default Home;
