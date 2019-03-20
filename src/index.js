eb145bedd6befa79a62d2301208de8c71d2b2a42
Hunter Warren <hunter.warren@gmail.com>
2019-03-19 22:52:06 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
