34124caab6ed89b279740e92300b29e59f33f178
Hunter Warren <hunter.warren@gmail.com>
2019-03-19 23:38:37 -0500

    security fixes and updates

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
-
 import { createStore } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <App />,
+const store = createStore(rootReducer);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
 
